e13a4b7636fe11ad63149bee5c9bf100eeaf79ce
ZhouChaoAlbert <838944042@qq.com>
2018-03-15 12:47:31 +0800

    obs1.0.1.4

--- build/config/miaversion.h
+++ build/config/miaversion.h
@@ -3,10 +3,10 @@
 
 #pragma execution_character_set("utf-8")
 
-#define MIA_VERSION   "1.0.1.3"
-#define MIA_VERNUM   1013
+#define MIA_VERSION   "1.0.1.4"
+#define MIA_VERNUM   1014
 
-#define VERSION   1,0,1,3
+#define VERSION   1,0,1,4
 #define FILE_VERSION MIA_VERSION
 #define COMPANYNAME "深圳市米亚印乐科技有限公司\0"
 #define LEGALCOPYRIGHT "版权所有（c）2016 米亚印乐\0"
